540a9465c090bdac4e453a44f0c079cdf534a1a4
Sfinford <96948227+Sfinford@users.noreply.github.com>
2023-11-24 17:23:47 -0700

    Add files via upload
    Restart button now functions like a previous button, going back to the previous song and storing songs in a history so you can go back multiple at once. Random button is much less likely to pick previously played songs until it has been a while since they were played.

--- script.js
+++ script.js
@@ -2,38 +2,54 @@ document.addEventListener('DOMContentLoaded', function() {
     const audioPlayer = document.getElementById('audio-player');
     const currentTrackElement = document.getElementById('current-track');
     let audioFiles = [];
-    let currentPlaying = null;
+    let history = [];
+    let maxHistoryLength = 0; // Variable to store the dynamic max history length
 
     document.getElementById('file-uploader').addEventListener('change', function(event) {
         audioFiles = event.target.files;
+        updateMaxHistoryLength();
         playAudioFile(0); // Play the first file initially
     });
 
     document.getElementById('restart-button').addEventListener('click', function() {
-        if (currentPlaying !== null) {
-            audioPlayer.currentTime = 0;
-            audioPlayer.play();
+        if (history.length > 1) {
+            history.pop(); // Remove current song from history
+            let previousIndex = history.pop(); // Get the previous song
+            playAudioFile(previousIndex);
         }
     });
 
     document.getElementById('random-button').addEventListener('click', function() {
         if (audioFiles.length > 0) {
-            let randomIndex = Math.floor(Math.random() * audioFiles.length);
+            let randomIndex;
+            do {
+                randomIndex = Math.floor(Math.random() * audioFiles.length);
+            } while (history.includes(randomIndex));
             playAudioFile(randomIndex);
         }
     });
 
     function playAudioFile(index) {
-        currentPlaying = index;
         let file = audioFiles[index];
         audioPlayer.src = URL.createObjectURL(file);
-        updateCurrentTrackDisplay(file.name); // Update the display
+        updateCurrentTrackDisplay(file.name);
         audioPlayer.play();
+        addToHistory(index);
     }
 
-function updateCurrentTrackDisplay(fileName) {
-        // Remove the file extension and update the display
+    function addToHistory(index) {
+        history.push(index);
+        if (history.length > maxHistoryLength) {
+            history.shift();
+        }
+    }
+
+    function updateCurrentTrackDisplay(fileName) {
         const displayName = fileName.replace(/\.[^/.]+$/, "");
         currentTrackElement.textContent = `${displayName}`;
+    }
+
+    function updateMaxHistoryLength() {
+        maxHistoryLength = Math.ceil(audioFiles.length * 0.8); // 20% reduction with ceiling
     }
 });
